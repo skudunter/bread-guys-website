654c003153a9e8a0a250b1745c7f6b131f2b772a
skudunter <danielgruttercpt@gmail.com>
2023-10-18 18:22:06 +0200

    feat added updateDB

--- app/api/handleSubmit/route.ts
+++ app/api/handleSubmit/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { RateLimiter } from "limiter";
+import updateDB from "./updateDB";
 const limiter = new RateLimiter({
   tokensPerInterval: 5,
   interval: "hour",
@@ -14,8 +15,8 @@ export async function POST(req: NextRequest, res: NextResponse) {
 //   if (email && mobileNumber && address && numberOfLoaves) {
     const remainingRequests = await limiter.removeTokens(1);
     if (remainingRequests > 0) {
+        updateDB(email, mobileNumber, address, numberOfLoaves)
       console.log("request made");
-
     } else {
       console.log("Tokens are up");
       return NextResponse.json({ status: 429 });
---
+++ app/api/handleSubmit/updateDB.ts
@@ -0,0 +1,3 @@
+export default function updateDB(email:string,mobileNumber:number,address:string,numberOfLoaves:number){
+
+}
